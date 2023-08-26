a1a47decfd292856d2ad8635d877bfcbfb45a54f
cvinicioyupa <38465363+sochoa1705@users.noreply.github.com>
2023-08-26 13:41:26 -0500

    contructor

--- src/main/java/ec/edu/espe/arquitectura/wsusuarios/model/ExternalRestModels/AccountRestRS.java
+++ src/main/java/ec/edu/espe/arquitectura/wsusuarios/model/ExternalRestModels/AccountRestRS.java
@@ -1,10 +1,14 @@
 package ec.edu.espe.arquitectura.wsusuarios.model.ExternalRestModels;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountRestRS {
 
     private String codeInternalAccount;
