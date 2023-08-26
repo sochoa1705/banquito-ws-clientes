9fe4f2b99993636b96dff91e0ee25826031fa0d0
Christopher <cramos@plugthem.social>
2023-08-26 12:33:58 -0500

    UNUSED

--- src/main/java/ec/edu/espe/arquitectura/wsusuarios/service/ExternalRestServices/AccountRestService.java
+++ src/main/java/ec/edu/espe/arquitectura/wsusuarios/service/ExternalRestServices/AccountRestService.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
